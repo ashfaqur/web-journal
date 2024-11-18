09d551700dd5c75751a65b945ec4f39e7541f5ec
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2024-11-18 05:10:57 +0100

    working client and server

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+	async function buttonAction() {
+		console.log('buttonAction');
+		try {
+			const response = await fetch('http://localhost:8181/items');
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			const data = await response.json();
+			console.log('Data:', data);
+		} catch (error) {
+			console.error('Fetch error:', error);
+		}
+	}
+</script>
+
 <h2 class="card-title">Hello World</h2>
+
+<button class="btn btn-primary" onclick={buttonAction}>Button</button>
